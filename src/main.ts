dcdbaaad1f3ef944c827f5949415862803d4b28d
Viih Cavalcante <viih.cavalcante@alu.ufc.br>
2025-05-28 00:03:39 +0000

    feat soluçao d tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
 // Escreva seu código aqui!
+const number = "2";
+const string = "Numero de ar condicionados do laboratório";
+const MeuCabeloÉLiso = true;
+const NumeroDePessoasDaSalaComCabeloCacheado = undefined
+const objeto = {notebook: "cinza, medio, 256 gigas"}
